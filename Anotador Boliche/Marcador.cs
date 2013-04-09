fb640b1b0f00b2cd27acb913b9d02d6b4d013063
Luis <luisl@simplytechinc.com>
2013-04-09 16:15:59 -0400

    Se crea la estructura de la clase Marcador

---
+++ Anotador Boliche/Marcador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anotador_Boliche
+{
+    class Marcador
+    {
+        struct Lanzamiento
+        {
+            public string lanzamiento;
+            public List<int> AcomuladosDependientes = new List<int>();
+        }
+        struct Turno
+        {
+            public List<Lanzamiento> Lanzamientos = new List<Lanzamiento>();
+            public int acomulado;
+        }
+        List<Turno> Turnos = new List<Turno>();
+        public Marcador(){}
+        private void nuevoTurno(Turno turno)
+        {
+
+        }
+        public void generarAcomulado()
+        {
+        
+        }
+        public void generarLanzamientos()
+        {
+
+        }
+    }
+}
