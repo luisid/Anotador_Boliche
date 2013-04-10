d43149a14c71c86b989aef8d30829ec4ffcfa1cb
Luis <luisl@simplytechinc.com>
2013-04-10 15:38:07 -0400

    Se completo Manejador
    Falta los unit Testing

---
+++ Anotador Boliche/Manejador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anotador_Boliche
+{
+    class Manejador
+    {
+        public Marcador PrimerJugador = new Marcador();
+        public Marcador SegundoJugador = new Marcador();
+        public Manejador(string FileLocation)
+        {
+            Split_Array_Lanzamientos(LeerArchivo.Leer_Archivo(FileLocation));
+        }
+        private void Split_Array_Lanzamientos(string[] arr)
+        {
+            string[] a = new string[21];
+            string[] b = new string[21];
+            for (int i = 0, j = 0, k = 0; i < 21; i++)
+            {
+                if (i < 18)
+                {
+                    a[j++] = arr[2 * i - (1 * (i % 2))];
+                    b[k++] = arr[2 * i - (1 * (i % 2)) + 2];
+                }
+                else
+                {
+                    a[j++] = arr[2 * i - i - 18];
+                    b[k++] = arr[2 * i + 21 - i];
+                }
+            }
+            PrimerJugador.generarLanzamientos(a);
+            SegundoJugador.generarLanzamientos(b);
+            PrimerJugador.generarAcumulado();
+            SegundoJugador.generarAcumulado();
+        }
+    }
+}
